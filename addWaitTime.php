eeccb2e509927666d49e7ba47f607b3fa50b6ee3
Zach Grant <zgrant28@gmail.com>
2015-09-16 13:32:37 -0600

    grrr 3

--- addWaitTime.php
+++ addWaitTime.php
@@ -2,13 +2,16 @@
 // Program to insert a row into the WaitTime table, when people are reporting wait times at a voting location
 // program call: addWaitTime.php?loc=nn&persons=nn&booth=nn&msg=xxxx -- where "nn" and "xxxx" are values passed
 // output is JSON [status & message]
+
 $servername = "mysql.code4abq.org";
 $username = "code4abqorg1";
 $password = "gjk%795h4#46";
 $dbname = "votinglocation";
+
 // list of approved user names (add more as needed)
 $approved = array("vote1", "vote2", "vote3");
 $arrlength = count($approved);
+
 // input field validation
 $loc = test_input($_GET["loc"]);
 if ($loc > "0" and $loc < "100") {
@@ -16,20 +19,17 @@ if ($loc > "0" and $loc < "100") {
 	$loc = 0;
 }
 
-
 $persons = test_input($_GET["persons"]);
 if ($persons > "0" and $persons < "1000") {
 } else {
 	$persons = 0;
 }
 
-
 $booth = test_input($_GET["booth"]);
 if ($booth > "0" and $booth < "100") {
 } else {
 	$booth = 0;
 }
-
 
 $approveduser = 0;
 for($x = 0; $x < $arrlength; $x++) {
@@ -61,8 +61,11 @@ if ($loc > 0 and $persons > 0) {
 } else {
 	$status = array('status' => 'error', 'message' => 'invalid data');
 }
+
 // send result as JSON sstring
 echo json_encode($status);
+
+
 // trim and strip bad data from input field
 function test_input($data) {
 	$data = trim($data);
@@ -70,4 +73,5 @@ function test_input($data) {
 	$data = htmlspecialchars($data);
 	return $data;
 }
+
 ?>
